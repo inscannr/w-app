9285fe49320ccd3295520008e9d80b2268d728a8
FlorjanKlancar <klancar.florjan99@gmail.com>
2023-01-26 15:10:10 +0100

    removed duplicate code

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -11,18 +11,19 @@ function Button({ children, buttonType, onClick }: Props) {
 
   switch (buttonType) {
     case "primary":
-      buttonStyle =
-        "w-full rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+      buttonStyle = "bg-indigo-600 text-white";
       break;
     case "secondary":
-      buttonStyle =
-        "w-full rounded-md border-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-indigo-600 border shadow-sm hover:bg-indigo-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+      buttonStyle = "border-indigo-600 text-indigo-600 border hover:text-white";
     default:
-      "w-full rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+      break;
   }
 
   return (
-    <button className={buttonStyle} onClick={onClick}>
+    <button
+      className={`w-full rounded-md px-3.5 py-1.5 text-base font-semibold leading-7 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${buttonStyle}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
